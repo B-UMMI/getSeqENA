a9ca06605e62aa63be80a9c3be7f682f92f31254
miguelpmachado <mpmachado@fc.ul.pt>
2017-08-08 10:38:40 +0100

    v1.1

--- getSeqENA.py
+++ getSeqENA.py
@@ -31,96 +31,98 @@ import sys
 import utils
 import time
 
-version = '1.0'
+version = '1.1'
 
 
 def requiredPrograms(args):
-	programs_version_dictionary = {}
-	if args.asperaKey is not None:
-		programs_version_dictionary['ascp'] = ['--version', '>=', '3.6.1']
-	if args.downloadCramBam:
-		programs_version_dictionary['samtools'] = ['--version', '==', '1.3.1']
-	missingPrograms = utils.checkPrograms(programs_version_dictionary)
-	if len(missingPrograms) > 0:
-		sys.exit('\n' + 'Errors:' + '\n' + '\n'.join(missingPrograms))
+    programs_version_dictionary = {}
+    if args.asperaKey is not None:
+        programs_version_dictionary['ascp'] = ['--version', '>=', '3.6.1']
+    if args.downloadCramBam:
+        programs_version_dictionary['samtools'] = ['--version', '==', '1.3.1']
+    missingPrograms = utils.checkPrograms(programs_version_dictionary)
+    if len(missingPrograms) > 0:
+        sys.exit('\n' + 'Errors:' + '\n' + '\n'.join(missingPrograms))
 
 
 def runGetSeqENA(args):
-	start_time = time.time()
+    start_time = time.time()
 
-	listENA_IDs = utils.getListIDs(os.path.abspath(args.listENAids.name))
-	outdir = os.path.abspath(args.outdir)
-	utils.check_create_directory(outdir)
-	asperaKey = args.asperaKey
-	if asperaKey is not None:
-		asperaKey = os.path.abspath(asperaKey.name)
+    listENA_IDs = utils.getListIDs(os.path.abspath(args.listENAids.name))
+    outdir = os.path.abspath(args.outdir)
+    utils.check_create_directory(outdir)
+    asperaKey = args.asperaKey
+    if asperaKey is not None:
+        asperaKey = os.path.abspath(asperaKey.name)
 
-	# Start logger
-	logfile = utils.start_logger(outdir)
+    # Start logger
+    logfile = utils.start_logger(outdir)
 
-	# Get general information
-	utils.general_information(logfile, version)
+    # Get general information
+    utils.general_information(logfile, version)
 
-	# Check programms
-	requiredPrograms(args)
+    # Check programms
+    requiredPrograms(args)
 
-	runs_successfully = 0
-	with open(os.path.join(outdir, 'getSeqENA.report.txt'), 'wt') as writer:
-		writer.write('\t'.join(['#ENA_ID', 'run_accession', 'run_successfully', 'downloadedFiles', 'instrument_platform', 'instrument_model', 'library_layout', 'library_source']) + '\n')
-		for ena_id in listENA_IDs:
-			if args.maximumSamples is None:
-				maximumSamples = runs_successfully + 1
-			else:
-				maximumSamples = args.maximumSamples
+    runs_successfully = 0
+    with open(os.path.join(outdir, 'getSeqENA.report.txt'), 'wt') as writer:
+        header_sequencing = ['run_accession', 'instrument_platform', 'instrument_model', 'library_layout', 'library_source', 'extra_run_accession', 'nominal_length', 'read_count', 'base_count', 'date_download']
+        writer.write('#sample' + '\t'.join(header_sequencing) + '\n')
+        for ena_id in listENA_IDs:
+            if args.maximumSamples is None:
+                maximumSamples = runs_successfully + 1
+            else:
+                maximumSamples = args.maximumSamples
 
-			if runs_successfully < maximumSamples:
-				print '\n' + 'Download ENA_ID ' + ena_id
+            if runs_successfully < maximumSamples:
+                print '\n' + 'Download ENA_ID ' + ena_id
 
-				ena_id_folder = os.path.join(outdir, ena_id)
-				utils.check_create_directory(ena_id_folder)
+                ena_id_folder = os.path.join(outdir, ena_id)
+                utils.check_create_directory(ena_id_folder)
 
-				run_successfully, downloadedFiles, sequencingInformation = download.runDownload(ena_id, args.downloadLibrariesType, asperaKey, ena_id_folder, args.downloadCramBam, args.threads, args.downloadInstrumentPlatform)
+                sequencingInformation = {'run_accession': None, 'instrument_platform': None, 'instrument_model': None, 'library_layout': None, 'library_source': None, 'extra_run_accession': None, 'nominal_length': None, 'read_count': None, 'base_count': None, 'date_download': None}
+                time_taken, run_successfully, fastq_files, sequencingInformation = download.runDownload(ena_id, args.downloadLibrariesType, asperaKey, ena_id_folder, args.downloadCramBam, args.threads, args.downloadInstrumentPlatform)
 
-				if run_successfully:
-					runs_successfully += 1
-				else:
-					utils.removeDirectory(ena_id_folder)
-					print ena_id + ' was not downloaded'
+                if run_successfully:
+                    runs_successfully += 1
+                else:
+                    utils.removeDirectory(ena_id_folder)
+                    print ena_id + ' was not downloaded'
 
-				writer.write('\t'.join([ena_id, str(sequencingInformation['run_accession']), str(run_successfully), str(';'.join(downloadedFiles) if downloadedFiles is not None else downloadedFiles), str(sequencingInformation['instrument_platform']), str(sequencingInformation['instrument_model']), str(sequencingInformation['library_layout']), str(sequencingInformation['library_source'])]) + '\n')
-			else:
-					break
+                writer.write(ena_id + '\t' + '\t'.join([str(sequencingInformation[i]) for i in header_sequencing]) + '\n')
+            else:
+                    break
 
-	time_taken = utils.runTime(start_time)
-	del time_taken
+    time_taken = utils.runTime(start_time)
+    del time_taken
 
-	if runs_successfully == 0:
-		sys.exit('No ENA_IDs were successfully downloaded!')
+    if runs_successfully == 0:
+        sys.exit('No ENA_IDs were successfully downloaded!')
 
 
 def main():
 
-	parser = argparse.ArgumentParser(prog='getSeqENA.py', description="Get fastq files from ENA using ENA IDs", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('--version', help='Version information', action='version', version=str('%(prog)s v' + version))
+    parser = argparse.ArgumentParser(prog='getSeqENA.py', description="Get fastq files from ENA using ENA IDs", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--version', help='Version information', action='version', version=str('%(prog)s v' + version))
 
-	parser_required = parser.add_argument_group('Required options')
-	parser_required.add_argument('-l', '--listENAids', type=argparse.FileType('r'), metavar='/path/to/list/ENA_IDs.txt', help='Path to list containing the ENA_IDs to be downloaded', required=True)
+    parser_required = parser.add_argument_group('Required options')
+    parser_required.add_argument('-l', '--listENAids', type=argparse.FileType('r'), metavar='/path/to/list/ENA_IDs.txt', help='Path to list containing the ENA_IDs to be downloaded', required=True)
 
-	parser_optional = parser.add_argument_group('Facultative options')
-	parser_optional.add_argument('-o', '--outdir', type=str, metavar='/output/directory/', help='Path for output directory', required=False, default='.')
-	parser_optional.add_argument('-j', '--threads', type=int, metavar='N', help='Number of threads', required=False, default=1)
-	parser_optional.add_argument('-a', '--asperaKey', type=argparse.FileType('r'), metavar='/path/to/asperaweb_id_dsa.openssh', help='Tells getSeqENA.py to download fastq files from ENA using Aspera Connect. With this option, the path to Private-key file asperaweb_id_dsa.openssh is provided (normaly found in ~/.aspera/connect/etc/asperaweb_id_dsa.openssh).', required=False)
-	parser_optional.add_argument('--downloadLibrariesType', type=str, metavar='PAIRED', help='Tells getSeqENA.py to download files with specific library layout', choices=['PAIRED', 'SINGLE', 'BOTH'], required=False, default='PAIRED')
-	parser_optional.add_argument('--downloadCramBam', action='store_true', help='Tells getSeqENA.py to also download cram/bam files and convert them to fastq files')
-	parser_optional.add_argument('--downloadInstrumentPlatform', type=str, metavar='ILLUMINA', help='Tells getSeqENA.py to download files with specific library layout', choices=['ILLUMINA', 'ALL'], required=False, default='ILLUMINA')
-	parser_optional.add_argument('--maximumSamples', type=int, metavar='N', help='Tells getSeqENA.py to only download files for N samples', required=False)
+    parser_optional = parser.add_argument_group('Facultative options')
+    parser_optional.add_argument('-o', '--outdir', type=str, metavar='/output/directory/', help='Path for output directory', required=False, default='.')
+    parser_optional.add_argument('-j', '--threads', type=int, metavar='N', help='Number of threads', required=False, default=1)
+    parser_optional.add_argument('-a', '--asperaKey', type=argparse.FileType('r'), metavar='/path/to/asperaweb_id_dsa.openssh', help='Tells getSeqENA.py to download fastq files from ENA using Aspera Connect. With this option, the path to Private-key file asperaweb_id_dsa.openssh is provided (normaly found in ~/.aspera/connect/etc/asperaweb_id_dsa.openssh).', required=False)
+    parser_optional.add_argument('--downloadLibrariesType', type=str, metavar='PAIRED', help='Tells getSeqENA.py to download files with specific library layout', choices=['PAIRED', 'SINGLE', 'BOTH'], required=False, default='BOTH')
+    parser_optional.add_argument('--downloadCramBam', action='store_true', help='Tells getSeqENA.py to also download cram/bam files and convert them to fastq files')
+    parser_optional.add_argument('--downloadInstrumentPlatform', type=str, metavar='ILLUMINA', help='Tells getSeqENA.py to download files with specific library layout', choices=['ILLUMINA', 'ALL'], required=False, default='ILLUMINA')
+    parser_optional.add_argument('--maximumSamples', type=int, metavar='N', help='Tells getSeqENA.py to only download files for N samples', required=False)
 
-	parser.set_defaults(func=runGetSeqENA)
+    parser.set_defaults(func=runGetSeqENA)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	args.func(args)
+    args.func(args)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- utils.py
+++ utils.py
@@ -8,195 +8,239 @@ import shutil
 from threading import Timer
 import functools
 import traceback
+import pickle
 
 
 def start_logger(workdir):
-	sys.stdout = Logger(workdir, time.strftime("%Y%m%d-%H%M%S"))
-	logfile = sys.stdout.getLogFile()
-	return logfile
+    sys.stdout = Logger(workdir, time.strftime("%Y%m%d-%H%M%S"))
+    logfile = sys.stdout.getLogFile()
+    return logfile
 
 
 class Logger(object):
-	def __init__(self, out_directory, time_str):
-		self.logfile = os.path.join(out_directory, str('run.' + time_str + '.log'))
-		self.terminal = sys.stdout
-		self.log = open(self.logfile, "w")
+    def __init__(self, out_directory, time_str):
+        self.logfile = os.path.join(out_directory, str('run.' + time_str + '.log'))
+        self.terminal = sys.stdout
+        self.log = open(self.logfile, "w")
 
-	def write(self, message):
-		self.terminal.write(message)
-		self.log.write(message)
-		self.log.flush()
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+        self.log.flush()
 
-	def flush(self):
-		pass
+    def flush(self):
+        pass
 
-	def getLogFile(self):
-		return self.logfile
+    def getLogFile(self):
+        return self.logfile
 
 
 def general_information(logfile, version):
-	# Check if output directory exists
+    # Check if output directory exists
 
-	print '\n' + '==========> getSeqENA <=========='
-	print '\n' + 'Program start: ' + time.ctime()
+    print '\n' + '==========> getSeqENA <=========='
+    print '\n' + 'Program start: ' + time.ctime()
 
-	# Tells where the logfile will be stored
-	print '\n' + 'LOGFILE:'
-	print logfile
+    # Tells where the logfile will be stored
+    print '\n' + 'LOGFILE:'
+    print logfile
 
-	# Print command
-	print '\n' + 'COMMAND:'
-	script_path = os.path.abspath(sys.argv[0])
-	print sys.executable + ' ' + script_path + ' ' + ' '.join(sys.argv[1:])
+    # Print command
+    print '\n' + 'COMMAND:'
+    script_path = os.path.abspath(sys.argv[0])
+    print sys.executable + ' ' + script_path + ' ' + ' '.join(sys.argv[1:])
 
-	# Print directory where programme was lunch
-	print '\n' + 'PRESENT DIRECTORY :'
-	present_directory = os.path.abspath(os.getcwd())
-	print present_directory
+    # Print directory where programme was lunch
+    print '\n' + 'PRESENT DIRECTORY :'
+    present_directory = os.path.abspath(os.getcwd())
+    print present_directory
 
-	# Print program version
-	print '\n' + 'VERSION:'
-	scriptVersionGit(version, present_directory, script_path)
+    # Print program version
+    print '\n' + 'VERSION:'
+    scriptVersionGit(version, present_directory, script_path)
 
-	# Print PATH variable
-	print '\n' + 'PATH variable:'
-	print os.environ['PATH']
+    # Print PATH variable
+    print '\n' + 'PATH variable:'
+    print os.environ['PATH']
 
 
 def scriptVersionGit(version, directory, script_path):
-	print 'Version ' + version
-	os.chdir(os.path.dirname(script_path))
-	command = ['git', 'log', '-1', '--date=local', '--pretty=format:"%h (%H) - Commit by %cn, %cd) : %s"']
-	run_successfully, stdout, stderr = runCommandPopenCommunicate(command, False, None, False)
-	print stdout
-	command = ['git', 'remote', 'show', 'origin']
-	run_successfully, stdout, stderr = runCommandPopenCommunicate(command, False, None, False)
-	print stdout
-	os.chdir(directory)
+    print 'Version ' + version
+    os.chdir(os.path.dirname(script_path))
+    command = ['git', 'log', '-1', '--date=local', '--pretty=format:"%h (%H) - Commit by %cn, %cd) : %s"']
+    run_successfully, stdout, stderr = runCommandPopenCommunicate(command, False, None, False)
+    print stdout
+    command = ['git', 'remote', 'show', 'origin']
+    run_successfully, stdout, stderr = runCommandPopenCommunicate(command, False, None, False)
+    print stdout
+    os.chdir(directory)
 
 
 def runTime(start_time):
-	end_time = time.time()
-	time_taken = end_time - start_time
-	hours, rest = divmod(time_taken, 3600)
-	minutes, seconds = divmod(rest, 60)
-	print 'Runtime :' + str(hours) + 'h:' + str(minutes) + 'm:' + str(round(seconds, 2)) + 's'
-	return time_taken
+    end_time = time.time()
+    time_taken = end_time - start_time
+    hours, rest = divmod(time_taken, 3600)
+    minutes, seconds = divmod(rest, 60)
+    print 'Runtime :' + str(hours) + 'h:' + str(minutes) + 'm:' + str(round(seconds, 2)) + 's'
+    return time_taken
 
 
 # USADO
 def check_create_directory(directory):
-	if not os.path.isdir(directory):
-		os.makedirs(directory)
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+
+def kill_subprocess_Popen(subprocess_Popen, command):
+    print 'Command run out of time: ' + str(command)
+    subprocess_Popen.kill()
 
 
 def runCommandPopenCommunicate(command, shell_True, timeout_sec_None, print_comand_True):
-	run_successfully = False
-	if isinstance(command, basestring):
-		command = shlex.split(command)
-	else:
-		command = shlex.split(' '.join(command))
+    run_successfully = False
+    if not isinstance(command, basestring):
+        command = ' '.join(command)
+    command = shlex.split(command)
 
-	if print_comand_True:
-		print 'Running: ' + ' '.join(command)
+    if print_comand_True:
+        print 'Running: ' + ' '.join(command)
 
-	if shell_True:
-		command = ' '.join(command)
-		proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	else:
-		proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	if timeout_sec_None is None:
-		stdout, stderr = proc.communicate()
-	else:
-		timer = Timer(timeout_sec_None, proc.kill)
-		timer.start()
-		stdout, stderr = proc.communicate()
-		timer.cancel()
+    if shell_True:
+        command = ' '.join(command)
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    else:
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-	if proc.returncode == 0:
-		run_successfully = True
-	else:
-		if not print_comand_True:
-			print 'Running: ' + str(command)
-		print 'STDOUT'
-		print stdout.decode("utf-8")
-		print 'STDERR'
-		print stderr.decode("utf-8")
-	return run_successfully, stdout, stderr
+    not_killed_by_timer = True
+    if timeout_sec_None is None:
+        stdout, stderr = proc.communicate()
+    else:
+        timer = Timer(timeout_sec_None, kill_subprocess_Popen, args=(proc, command,))
+        timer.start()
+        stdout, stderr = proc.communicate()
+        timer.cancel()
+        not_killed_by_timer = timer.isAlive()
+
+    if proc.returncode == 0:
+        run_successfully = True
+    else:
+        if not print_comand_True and not_killed_by_timer:
+            print 'Running: ' + str(command)
+        if len(stdout) > 0:
+            print 'STDOUT'
+            print stdout.decode("utf-8")
+        if len(stderr) > 0:
+            print 'STDERR'
+            print stderr.decode("utf-8")
+    return run_successfully, stdout, stderr
 
 
 # Remove directory
 def removeDirectory(directory):
-	if os.path.isdir(directory):
-		shutil.rmtree(directory)
+    if os.path.isdir(directory):
+        shutil.rmtree(directory)
 
 
 # USADO
 def getListIDs(fileListIDs):
-	list_ids = []
+    list_ids = []
 
-	with open(fileListIDs, 'rtU') as lines:
-		for line in lines:
-			line = line.splitlines()[0]
-			if len(line) > 0:
-				list_ids.append(line)
+    with open(fileListIDs, 'rtU') as lines:
+        for line in lines:
+            line = line.splitlines()[0]
+            if len(line) > 0:
+                list_ids.append(line)
 
-	if len(list_ids) == 0:
-		sys.exit('No runIDs were found in ' + fileListIDs)
+    if len(list_ids) == 0:
+        sys.exit('No runIDs were found in ' + fileListIDs)
 
-	return list_ids
+    return list_ids
 
 
 # Check programs versions
 def checkPrograms(programs_version_dictionary):
-	print '\n' + 'Checking dependencies...'
-	programs = programs_version_dictionary
-	which_program = ['which', '']
-	listMissings = []
-	for program in programs:
-		which_program[1] = program
-		run_successfully, stdout, stderr = runCommandPopenCommunicate(which_program, False, None, False)
-		if not run_successfully:
-			listMissings.append(program + ' not found in PATH.')
-		else:
-			if programs[program][0] is None:
-				print program + ' (impossible to determine programme version) found at: ' + stdout.splitlines()[0]
-			else:
-				check_version = [stdout.splitlines()[0], programs[program][0]]
-				run_successfully, stdout, stderr = runCommandPopenCommunicate(check_version, False, None, False)
-				if stdout == '':
-					stdout = stderr
-				if program == 'bunzip2':
-					version_line = stdout.splitlines()[0].rsplit(',', 1)[0].split(' ')[-1]
-				else:
-					version_line = stdout.splitlines()[0].split(' ')[-1]
-				replace_characters = ['"', 'v', 'V', '+']
-				for i in replace_characters:
-					version_line = version_line.replace(i, '')
-				print program + ' (' + version_line + ') found'
-				if programs[program][1] == '>=':
-					program_found_version = version_line.split('.')
-					program_version_required = programs[program][2].split('.')
-					if float('.'.join(program_found_version[0:2])) < float('.'.join(program_version_required[0:2])):
-						listMissings.append('It is required ' + program + ' with version ' + programs[program][1] + ' ' + programs[program][2])
-					elif float('.'.join(program_found_version[0:2])) == float('.'.join(program_version_required[0:2])):
-						if len(program_version_required) == 3:
-							if len(program_found_version) == 2:
-								program_found_version.append(0)
-							if program_found_version[2].split('_')[0] < program_version_required[2]:
-								listMissings.append('It is required ' + program + ' with version ' + programs[program][1] + ' ' + programs[program][2])
-				else:
-					if version_line != programs[program][2]:
-						listMissings.append('It is required ' + program + ' with version ' + programs[program][1] + ' ' + programs[program][2])
-	return listMissings
+    print '\n' + 'Checking dependencies...'
+    programs = programs_version_dictionary
+    which_program = ['which', '']
+    listMissings = []
+    for program in programs:
+        which_program[1] = program
+        run_successfully, stdout, stderr = runCommandPopenCommunicate(which_program, False, None, False)
+        if not run_successfully:
+            listMissings.append(program + ' not found in PATH.')
+        else:
+            if programs[program][0] is None:
+                print program + ' (impossible to determine programme version) found at: ' + stdout.splitlines()[0]
+            else:
+                check_version = [stdout.splitlines()[0], programs[program][0]]
+                run_successfully, stdout, stderr = runCommandPopenCommunicate(check_version, False, None, False)
+                if stdout == '':
+                    stdout = stderr
+                if program == 'bunzip2':
+                    version_line = stdout.splitlines()[0].rsplit(',', 1)[0].split(' ')[-1]
+                else:
+                    version_line = stdout.splitlines()[0].split(' ')[-1]
+                replace_characters = ['"', 'v', 'V', '+']
+                for i in replace_characters:
+                    version_line = version_line.replace(i, '')
+                print program + ' (' + version_line + ') found'
+                if programs[program][1] == '>=':
+                    program_found_version = version_line.split('.')
+                    program_version_required = programs[program][2].split('.')
+                    if float('.'.join(program_found_version[0:2])) < float('.'.join(program_version_required[0:2])):
+                        listMissings.append('It is required ' + program + ' with version ' + programs[program][1] + ' ' + programs[program][2])
+                    elif float('.'.join(program_found_version[0:2])) == float('.'.join(program_version_required[0:2])):
+                        if len(program_version_required) == 3:
+                            if len(program_found_version) == 2:
+                                program_found_version.append(0)
+                            if program_found_version[2].split('_')[0] < program_version_required[2]:
+                                listMissings.append('It is required ' + program + ' with version ' + programs[program][1] + ' ' + programs[program][2])
+                else:
+                    if version_line != programs[program][2]:
+                        listMissings.append('It is required ' + program + ' with version ' + programs[program][1] + ' ' + programs[program][2])
+    return listMissings
 
 
 def trace_unhandled_exceptions(func):
-	@functools.wraps(func)
-	def wrapped_func(*args, **kwargs):
-		try:
-			func(*args, **kwargs)
-		except:
-			print 'Exception in ' + func.__name__
-			traceback.print_exc()
-	return wrapped_func
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except:
+            print 'Exception in ' + func.__name__
+            traceback.print_exc()
+    return wrapped_func
+
+
+def saveVariableToPickle(variableToStore, outdir, prefix):
+    pickleFile = os.path.join(outdir, str(prefix + '.pkl'))
+    with open(pickleFile, 'wb') as writer:
+        pickle.dump(variableToStore, writer)
+
+
+def extractVariableFromPickle(pickleFile):
+    with open(pickleFile, 'rb') as reader:
+        variable = pickle.load(reader)
+    return variable
+
+
+def rchop(string, ending):
+    if string.endswith(ending):
+        string = string[:-len(ending)]
+    return string
+
+
+def timer(function, name):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        print('\n' + 'RUNNING {0}\n'.format(name))
+        start_time = time.time()
+
+        results = list(function(*args, **kwargs))  # guarantees return is a list to allow .insert()
+
+        time_taken = runTime(start_time)
+        print('END {0}'.format(name))
+
+        results.insert(0, time_taken)
+        return results
+    return wrapper
